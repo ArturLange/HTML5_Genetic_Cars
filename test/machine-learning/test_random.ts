3b8057c5ca1a22147ed09f049bf8beef7bccff8c
Artur Lange <arturlange@poczta.fm>
2017-11-27 15:07:36 +0100

    Add coverage from nyc

---
+++ test/machine-learning/test_random.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import 'mocha';
+import { mapToShuffle } from '../../src/machine-learning/random';
+
+//describe('mapToShuffle function', () => {
+//    it('should return predefined values', () => {
+//        const prop = {};
+//        const normals = {};
+//        expect(mapToShuffle(prop, normals)).to.deep.equal(true);
+//    });
+//});
